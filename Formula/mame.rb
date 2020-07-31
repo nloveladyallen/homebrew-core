fafca020eae8b25d016196ccf8bfab32b3347625
Rui Chen <rui@meetup.com>
2020-07-31 11:43:46 -0400

    mame: revision for lua

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -5,6 +5,7 @@ class Mame < Formula
   version "0.224"
   sha256 "3518e71ec20fbeac8ebe93f8ec856078b8288e19f0d7cb38959d4bde30cd2810"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/mamedev/mame.git"
 
   bottle do
